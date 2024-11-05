6f480cd595b4578c9ace5618017cf1b6bde0959b
glutonium69 <tahzib404@gmail.com>
2024-11-06 02:27:04 +0600

    Added: shell prompt modification to show repo info

--- .custom_func.sh
+++ .custom_func.sh
@@ -38,3 +38,19 @@ cf() {
     fi
 }
 
+
+# Function to get the current Git branch and status. using it to modify the shell prompt
+function parse_git_branch {
+    # Get the current branch name
+    branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    if [ -n "$branch" ]; then
+        # Check for uncommitted changes
+        if [ -n "$(git status --porcelain)" ]; then
+            # Uncommitted changes detected
+	    printf "\e[0m\e[1;91m[$branch ✗]\e[0m"
+        else
+            # Clean working tree
+            printf "\e[0m\e[1;92m[$branch ✓]\e[0m"
+        fi
+    fi
+}
