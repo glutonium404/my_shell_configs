3b0a349eae1cafdf7d7ee422e40d7d346449b487
glutonium69 <tahzib404@gmail.com>
2024-11-12 21:00:10 +0600

    Added: key binding. move through command history with \C-{j,k}

--- .custom_key_bind.sh
+++ .custom_key_bind.sh
@@ -2,7 +2,14 @@
 ##########     custom key binding   ##############
 ##################################################
 
-bind '"\C-H": kill-whole-line' # delete whole line when ctrl + backspace is pressed
+# delete whole line when ctrl + backspace is pressed
+bind '"\C-H": kill-whole-line'
+
 # bind -x '"\C-f":"cd $(find ~/ -type d | fzf) && pwd"' # lets u move to any dir using fuzzy finder. hit ctrl + f then select your dir
 
-bind -x '"\C-f":"cd $(cf)"' # lets u move to any dir using fuzzy finder. hit ctrl + f then select your dir
+# lets u move to any dir using fuzzy finder. hit ctrl + f then select your dir
+bind -x '"\C-f":"cd $(cf)"'
+
+# moving between the command history using j and k to keep things related to vim key binding
+bind '"\C-j": history-search-forward'
+bind '"\C-k": history-search-backward'
