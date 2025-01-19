50ea5f766f6663961a0e1a6b251df6adffb6546f
glutonium69 <tahzib404@gmail.com>
2025-01-19 18:54:27 +0600

    Some minor update

--- .custom.sh
+++ .custom.sh
@@ -2,6 +2,7 @@ alias cls='clear && ls;'
 alias clcd='clear && cd'
 alias dir='dir --color=always'
 alias obsidian='/opt/Obsidian/obsidian'
+alias catf='cat $(fzf)'
 
 source "$HOME/.shell_custom_config/.custom_func.sh"
 source "$HOME/.shell_custom_config/.custom_key_bind.sh"
