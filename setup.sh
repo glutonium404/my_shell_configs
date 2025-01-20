b4f50e7c738bd6d988cc7d31c2c35b97391b8776
glutonium69 <tahzib404@gmail.com>
2025-01-20 21:58:59 +0600

    Adding fzf to installation packages

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt upgrade
 echo "==============="
 echo "installing pkgs"
 echo "==============="
-sudo apt install tmux gh git neovim make gcc ripgrep unzip xclip curl
+sudo apt install tmux gh git neovim make gcc ripgrep unzip xclip curl fzf
 
 echo "============="
 echo "setting up gh"
