fb75e6c89d9abfa869757f732790024a8a7dc970
glutonium69 <tahzib404@gmail.com>
2025-01-21 20:18:45 +0600

    minor fix

--- .custom_key_bind.sh
+++ .custom_key_bind.sh
@@ -8,7 +8,7 @@ bind '"\C-H": kill-whole-line'
 # bind -x '"\C-f":"cd $(find ~/ -type d | fzf) && pwd"' # lets u move to any dir using fuzzy finder. hit ctrl + f then select your dir
 
 # lets u move to any dir using fuzzy finder. hit ctrl + f then select your dir
-bind -x '"\C-f":"cd $(cf)"'
+bind -x '"\C-f":"cf"'
 
 # moving between the command history using j and k to keep things related to vim key binding
 bind '"\C-j": history-search-forward'
